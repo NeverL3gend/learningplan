7444c8dc809b18781d2e115ca31f9db08ff0fffe
Neverl3gend <alejandrogarcicojulun@gmail.com>
2021-02-03 21:02:41 -0500

    learning how to interact with the DOM using JS

---
+++ Side project/main.js
@@ -0,0 +1,12 @@
+//Every time I click in the button I want to change the color
+// let btnPrimary = document.querySelector('#primary')
+// let btnSecondary = document.querySelector('#danger')
+// let btnTertiary = document.querySelector('#success')
+
+// btnPrimary.addEventListener('click', ()=> btnPrimary.style.backgroundColor = 'blue')
+// btnSecondary.addEventListener('click', ()=> btnSecondary.style.backgroundColor = 'red')
+// btnTertiary.addEventListener('click', ()=> btnTertiary.style.backgroundColor = 'green')
+
+let btnChange = document.querySelector('#change')
+
+btnChange.addEventListener('click', () => box.style.backgroundColor = 'red')
